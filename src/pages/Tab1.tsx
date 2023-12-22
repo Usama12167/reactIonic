20ad09a597cbdeda95505a0b75e92fe3726a8961
usama_gsoft <usama@gsoftconsulting.com>
2023-12-22 11:17:34 +0500

    run app

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,54 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonImg, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { camera, trash, close } from 'ionicons/icons';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Plugins } from '@capacitor/core';
+import { useState } from 'react';
 
 const Tab1: React.FC = () => {
+
+  const { Camera } = Plugins;
+  const [photo, setPhoto] = useState();
+  const takePhoto = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+    });
+    setPhoto(image.webPath);
+  };
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Take a photo </IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={takePhoto}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
+  <IonGrid>
+    <IonRow>
+      {/* {photo.map((photos, index) => (
+        <IonCol size="6" key={photo.filepath}> */}
+          <IonImg src={photo} />
+        {/* </IonCol> */}
+      {/* ))} */}
+    </IonRow>
+  </IonGrid>
       </IonContent>
+
     </IonPage>
   );
 };
